866b42a79ad52d8e1c6237e33b95c7ff1f44eb3d
Halloween <44981960+Phantasy190@users.noreply.github.com>
2023-09-25 00:57:44 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import 'zone.js';
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+
+import 'zone.js';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
